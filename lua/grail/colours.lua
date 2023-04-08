3bbd58094929d6ec1f0eeacaf2d2e02b1934a983
Peter Kuchar <you@example.com>
2023-04-08 15:38:18 +0200

    fix color for builtin types

--- lua/grail/colours.lua
+++ lua/grail/colours.lua
@@ -1,61 +1,61 @@
 local M = {}
 
 local hard_background = {
-  dark = {
-    bg_dim = "#191E20",
-    bg0 = "#191E20",
-    bg1 = "#21272A",
-    bg2 = "#282F32",
-    bg3 = "#30383C",
-    bg4 = "#394247",
-    bg5 = "#3F494E",
-    bg_visual = "#282f35",
-    bg_red = "#493b40",
-    bg_green = "#3f4841",
-    bg_blue = "#384b55",
-    bg_yellow = "#45443c",
-  },
+	dark = {
+		bg_dim = "#191E20",
+		bg0 = "#191E20",
+		bg1 = "#21272A",
+		bg2 = "#282F32",
+		bg3 = "#30383C",
+		bg4 = "#394247",
+		bg5 = "#3F494E",
+		bg_visual = "#282f35",
+		bg_red = "#493b40",
+		bg_green = "#3f4841",
+		bg_blue = "#384b55",
+		bg_yellow = "#45443c",
+	},
 }
 
 local base_palette = {
-  dark = {
-    fg = "#B4B5A7",
-    fgdark = "#B19565",
-    pink1 = "#B04D4E",
-    red = "#DF5B51",
-    orange = "#B17B1E",
-    yellow = "#C4B64F",
-    yellowlight = "#C0BF45",
-    green = "#85995E",
-    green2 = "#3C6055",
-    green3 = "#96B38F",
-    pinkdark = "#B29A69",
-    pinklight = "#E65154",
-    aqua = "#33B1A6",
-    blue = "#338E9F",
-    blue2 = "#295972",
-    blue3 = "#44728A",
-    blue4 = "#628B96",
-    blue5 = "#819EAD",
-    purple = "#7075B2",
-    purpledark = "#836AA3",
-    grey = "#4F5045",
-    grey0 = "#616255",
-    grey1 = "#6C6D5F",
-    grey2 = "#797A6A",
-    grey3 = "#828372",
-    grey4 = "#8B8C7A",
-    grey5 = "#979985",
-    grey6 = "#A0A28D",
-    grey7 = "#A7A993",
-    grey8 = "#B0B29B",
-    grey9 = "#BABCA4",
-    grey10 = "#C5C7AD",
-    statusline1 = "#6EBB9F",
-    statusline2 = "#D3B980",
-    statusline3 = "#E65255",
-    none = "NONE",
-  },
+	dark = {
+		fg = "#B4B5A7",
+		fgdark = "#B19565",
+		pink1 = "#B04D4E",
+		red = "#DF5B51",
+		orange = "#B17B1E",
+		yellow = "#ACA67D",
+		yellowlight = "#C0BF45",
+		green = "#85995E",
+		green2 = "#3C6055",
+		green3 = "#96B38F",
+		pinkdark = "#B29A69",
+		pinklight = "#E65154",
+		aqua = "#33B1A6",
+		blue = "#338E9F",
+		blue2 = "#295972",
+		blue3 = "#44728A",
+		blue4 = "#628B96",
+		blue5 = "#819EAD",
+		purple = "#7075B2",
+		purpledark = "#836AA3",
+		grey = "#4F5045",
+		grey0 = "#616255",
+		grey1 = "#6C6D5F",
+		grey2 = "#797A6A",
+		grey3 = "#828372",
+		grey4 = "#8B8C7A",
+		grey5 = "#979985",
+		grey6 = "#A0A28D",
+		grey7 = "#A7A993",
+		grey8 = "#B0B29B",
+		grey9 = "#BABCA4",
+		grey10 = "#C5C7AD",
+		statusline1 = "#6EBB9F",
+		statusline2 = "#D3B980",
+		statusline3 = "#E65255",
+		none = "NONE",
+	},
 }
 
 ---@class Palette
@@ -113,12 +113,12 @@ local base_palette = {
 ---@param theme "light" | "dark" The user's background preference
 ---@return Palette
 M.generate_palette = function(options, theme)
-  local base = base_palette[theme]
-  local background_palette
+	local base = base_palette[theme]
+	local background_palette
 
-  background_palette = hard_background[theme]
+	background_palette = hard_background[theme]
 
-  return vim.tbl_extend("force", base, background_palette)
+	return vim.tbl_extend("force", base, background_palette)
 end
 
 return M
